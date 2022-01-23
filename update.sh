87a85ba6970ef971e54c7133e0f2ef53cc6aac65
rrvq <rrvq@example.com>
2022-01-23 03:43:43 +0000

    update

--- update.sh
+++ update.sh
@@ -4,8 +4,9 @@ set -ex
 
 git config user.email "rrvq@example.com"
 git config user.name "rrvq"
-wget -6 -nc -O devmoe https://dev.moe/feed
-wget -6 -nc -O atri https://blog.atri.tk/atom.xml
+wget -6 -O devmoe https://dev.moe/feed
+wget -6 -O atri https://blog.atri.tk/atom.xml
+wget -6 -O kiki https://kirikira.moe/rss/
 git add .
 git commit -m 'update'
 git push
