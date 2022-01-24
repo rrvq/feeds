489682faf88eea33422718c480703e54e52cacfa
rrvq <rrvq@example.com>
2022-01-24 09:37:08 +0000

    check diff size

--- update.sh
+++ update.sh
@@ -6,7 +6,16 @@ git config user.email "rrvq@example.com"
 git config user.name "rrvq"
 wget -6 -O devmoe https://dev.moe/feed
 wget -6 -O atri https://blog.atri.tk/atom.xml
-wget -6 -O kiki https://kirikira.moe/rss/
+
+# this feed gets rebuild frequently, cmp print different bytes, less than 4(rebuild time) then ignore
+wget -6 -O kiki-new https://kirikira.moe/rss/
+if [ $(cmp -l kiki-new kiki | wc -l) -le 10 ]
+then
+    rm kiki-new
+else
+    mv kiki-new kiki
+fi
+       
 git add .
 git commit -m 'update'
 git push
